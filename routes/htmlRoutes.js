12f2d8977cf0fc0d0e7a9c81b0ddfc1949ef7081
IMXMOLINA <imxmolina@gmail.com>
2018-08-15 19:55:17 -0700

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,17 @@
-var db = require("../models");
+var path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/user-view.html"));
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    app.get("/all", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/all.html"));
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+
+    app.get("/add", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/add.html"));
+    });
+
+}
